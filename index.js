c4ff3a015e0b9aa232a777234a54a72681c30347
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2021-11-30 20:27:29 +0100

    Update index.js
    If applied, this commit will have code for authenticated end-points.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ bodyParser = require('body-parser'),
 Models = require('./model.js'),
 Movies = Models.Movie,
 Users = Models.User;
+const passport = require('passport');
+require('./passport');
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Middleware
@@ -14,6 +16,8 @@ app.use(morgan('common'));
 app.use((err, req, res, next) => {console.error(err.stack);res.status(500).send('Something broke!');});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+let auth = require('./auth')(app);
 
 // HTTP requests
 app.get('/', (req, res) => {
@@ -25,7 +29,7 @@ app.get('/documentation', (req, res) => {
 });
 
 //Return a list of ALL movies to the user
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(201).json(movies);
@@ -37,7 +41,7 @@ app.get('/movies', (req, res) => {
 });
 
 //Return data about a single movie by title to the user
-app.get('/movies/:Title', (req, res) => {
+app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.findOne({ Title: req.params.Title })
   .then((movie) => {
     res.json(movie);
@@ -49,7 +53,7 @@ app.get('/movies/:Title', (req, res) => {
 });
 
 //Return the list of all movies by genre to the user
-app.get('/movies/Genre/:Name', (req, res) => {
+app.get('/movies/Genre/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find({ "Genre.Name" : req.params.Name })
   .then((movie) => {
     res.json(movie);
@@ -61,7 +65,7 @@ app.get('/movies/Genre/:Name', (req, res) => {
 });
 
 //Return data about a director by name
-app.get('/movies/directors/:Name', (req, res) => {
+app.get('/movies/directors/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find({ "Director.Name" : req.params.Name },{"Director.$" : 1})
   .then((Director) => {
     res.status(201).json(Director);
@@ -73,7 +77,7 @@ app.get('/movies/directors/:Name', (req, res) => {
 });
 
 //Return a list of all users
-app.get('/users', (req, res) => {
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.find()
   .then((user) => {
     res.status(201).json(user);
@@ -85,8 +89,8 @@ app.get('/users', (req, res) => {
 });
 
 // Return a user by username
-app.get('/users/:Name', (req, res) => {
-  Users.findOne({ Name: req.params.Name })
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.params.Username })
   .then((user) => {
     res.json(user);
   })
@@ -97,14 +101,14 @@ app.get('/users/:Name', (req, res) => {
 });
 
 //Allow new users to register
-app.post('/users', (req, res) => {
-  Users.findOne({ Name: req.body.Name })
+app.post('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.body.Username })
   .then((user) => {
     if (user) {
-      return res.status(400).send(req.body.Name + 'already exists');
+      return res.status(400).send(req.body.Username + ' already exists');
     } else {
       Users.create({
-        Name: req.body.Name,
+        Username: req.body.Username,
         Password: req.body.Password,
         EmailID: req.body.EmailID,
         Birth: req.body.Birth
@@ -123,10 +127,10 @@ app.post('/users', (req, res) => {
 });
 
 //Allow users to update their user info (Username,Password,EmailID,Birthday)
-app.put('/users/:Name', (req, res) => {
-  Users.findOneAndUpdate({ Name: req.params.Name }, { $set:
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
-      Name: req.body.Name,
+      Username: req.body.Username,
       Password: req.body.Password,
       EmailID: req.body.EmailID,
       Birth: req.body.Birth
@@ -144,8 +148,8 @@ app.put('/users/:Name', (req, res) => {
 });
 
 //Allow users to add a movie to their list of favorites
-app.post('/users/:Name/movies/:MovieID', (req, res) => {
-  Users.findOneAndUpdate({ Name: req.params.Name }, {
+app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
     $push: { FavouriteMovies: req.params.MovieID }
   },
   { new: true }, // This line makes sure that the updated document is returned
@@ -160,13 +164,13 @@ app.post('/users/:Name/movies/:MovieID', (req, res) => {
 });
 
 //Allow existing users to deregister
-app.delete('/users/delete/:Name', (req, res) => {
-  Users.findOneAndRemove({ Name: req.params.Name })
+app.delete('/users/delete/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
   .then((user) => {
     if (!user) {
-      res.status(400).send(req.params.Name + ' was not found');
+      res.status(400).send(req.params.Username + ' was not found');
     } else {
-      res.status(200).send(req.params.Name + ' was deleted.');
+      res.status(200).send(req.params.Username + ' was deleted.');
     }
   })
   .catch((err) => {
@@ -177,8 +181,8 @@ app.delete('/users/delete/:Name', (req, res) => {
 
 
 //Allow users to remove a movie from their list of favorites
-app.delete('/users/:Name/delete/:MovieID', (req, res) => {
-  Users.findOneAndUpdate({ Name: req.params.Name }, {
+app.delete('/users/:Username/delete/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
     $pull: { FavouriteMovies: req.params.MovieID }
   },
   { new: true }, // This line makes sure that the updated document is returned
