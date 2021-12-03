ae382067ae6cc5851abb554d37a86e062916f9e7
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2021-12-03 14:13:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ Users = Models.User,
 cors = require('cors'),
 passport = require('passport');
 require('./passport');
-//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('process.env.CONNECTION_URI',{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('process.env.CONNECTION_URI',{ useNewUrlParser: true, useUnifiedTopology: true });
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 const { check, validationResult } = require('express-validator');
