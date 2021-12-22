18d73d25b6645b7c278893c3f171423dd750eb1c
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2021-12-22 21:32:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,23 @@ const express = require('express'),
   cors = require('cors'),
   passport = require('passport');
 require('./passport');
-let allowedOrigins = ['http://localhost:1234','https://www.filmaffinity.com/us/movieimage.php?imageId=746000499'];
+let allowedOrigins = ['*'];
 const { check, validationResult } = require('express-validator');
 //Mongoose Atlas Database connection string
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Middleware
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+app.use(cors());
 app.use(express.static('public'));
 app.use(morgan('common'));
 app.use((err, req, res, next) => { console.error(err.stack); res.status(500).send('Something broke!'); });
