5aa051ab9573a2ff5e50ed4a30671f8209f7196b
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2021-12-22 20:07:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ const express = require('express'),
   cors = require('cors'),
   passport = require('passport');
 require('./passport');
-let allowedOrigins = ['*'];
+let allowedOrigins = ['*','https://www.filmaffinity.com'];
 const { check, validationResult } = require('express-validator');
 //Mongoose Atlas Database connection string
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Middleware
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(express.static('public'));
 app.use(morgan('common'));
 app.use((err, req, res, next) => { console.error(err.stack); res.status(500).send('Something broke!'); });
