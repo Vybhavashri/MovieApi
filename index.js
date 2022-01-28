3fd5546975d87b26f5fe7a5796e967662c9ef998
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2022-01-28 17:32:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 });
 
 //Allow users to add a movie to their list of favorites
-app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username }, {
     $push: { FavouriteMovies: req.params.MovieID }
   },
